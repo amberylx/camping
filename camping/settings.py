54dee9aae6259d480742ef96a35a6ada196748d6
amberylx <amberyl.x@gmail.com>
2020-03-07 20:45:52 -0800

    Adds postgres database settings and psycopg package

--- camping/settings.py
+++ camping/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'camping.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'camping',
+        'USER': 'slee',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
