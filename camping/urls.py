a723578bdf505adf6c58320d09341d750c9b912e
amberylx <amberyl.x@gmail.com>
2020-03-11 22:13:21 -0700

    Splits out camping-app-specific urls.py

--- camping/urls.py
+++ camping/urls.py
@@ -4,6 +4,5 @@ from inventory import views
 
 
 urlpatterns = [
-    # path('inventory/', include('inventory.urls')),
-    path(r'inventory/', views.inventory_list, name='inventory_list'),
+    path('inventory/', include('inventory.urls')),
 ]
