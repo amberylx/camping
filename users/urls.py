6f969b27697847fdf11d5137f3ecf785b21e08a4
amberylx <amberyl.x@gmail.com>
2020-03-13 14:24:48 -0700

    Switches to using django provided auth views

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from users import views
 
 
 urlpatterns = [
-    path(r'login', views.login, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login")
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,1 @@
 from django.shortcuts import render
-
-from users import models
-
-
-def login(request):
-    return render(request, 'login.html', {
-    }, content_type='text/html')
