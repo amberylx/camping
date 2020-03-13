141f905185fc640ca0b51fc4f9db551188324b80
papir805 <papir805@gmail.com>
2020-03-13 16:00:25 -0700

    Moved CampingTrip model to trips app

--- inventory/models.py
+++ inventory/models.py
@@ -1,12 +1,8 @@
 from django.db import models
+from trips.models import CampingTrip
 
 
-class CampingTrip(models.Model):
-    start_date = models.DateField(name='start_date') 
-    end_date = models.DateField(name='end_date')
-    name = models.CharField(max_length=100, blank=True)
-    park_name = models.CharField(max_length=100)
-    #camp_item = models.ForeignKey(to=CampItem, on_delete=models.CASCADE, null=False) 
+
     
     
 class CampItem(models.Model):
