00610bc3cc136273712e8f886d5b80f2786a152a
papir805 <papir805@gmail.com>
2020-03-13 15:48:32 -0700

    Added CampingTrip model with foreign key

--- inventory/models.py
+++ inventory/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 
+class CampingTrip(models.Model):
+    start_date = models.DateField(name='start_date') 
+    end_date = models.DateField(name='end_date')
+    name = models.CharField(max_length=100, blank=True)
+    park_name = models.CharField(max_length=100)
+    #camp_item = models.ForeignKey(to=CampItem, on_delete=models.CASCADE, null=False) 
+    
+    
 class CampItem(models.Model):
     name = models.CharField(max_length=200, blank=False)
     quantity = models.PositiveIntegerField(blank=False)
+    trip = models.ForeignKey(to=CampingTrip, on_delete = models.CASCADE, null=False)
+    
+ 
